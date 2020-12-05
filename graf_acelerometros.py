bfad37ede61b52e30f9fa3250077e1c657547209
Francisco <pagolo2000@gmail.com>
2020-12-05 16:57:08 +0100

    sin implementar boton b

---
+++ graf_acelerometros.py
@@ -0,0 +1,48 @@
+from microbit import *
+
+MAX = 10
+
+def max (maximo,posibleMaximo):
+    if maximo < posibleMaximo:
+        maximo = posibleMaximo
+    return maximo
+
+def min (minimo,posibleMinimo):
+    if minimo > posibleMinimo:
+        minimo = posibleMinimo
+    return minimo
+
+def main():
+        readings = [(0,0,0)] * MAX
+        while True:
+            valoresx = 0
+            valoresy = 0
+            valoresz = 0
+            n = 1
+            for i in range(MAX):
+                x = accelerometer.get_x()
+                y = accelerometer.get_y()
+                z = accelerometer.get_z()
+                tupla = (x,y,z)
+                readings[i] = tupla
+                valoresx += x
+                valoresy += y
+                valoresz += z
+                mediax = valoresx / n
+                mediay = valoresy / n
+                mediaz = valoresz / n
+                print((mediax,mediay,mediaz))
+                sleep(100)
+                n += 1
+            if button_a.is_pressed():
+                maximox = -2000
+                maximoy = -2000
+                maximoz = -2000
+                for k in range(MAX):
+                    maximox = max(maximox,(readings[k][0]))
+                    maximoy = max(maximoy,(readings[k][1]))
+                    maximoz = max(maximoz, (readings[k][2]))
+                display.scroll(" "+str(maximox)+" " + str(maximoy)+ " " + str(maximoz))
+
+if __name__ == "__main__":
+    main()
